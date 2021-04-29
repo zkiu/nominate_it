f1a31087a44f261f0d7d2218239e9cc53c684a85
Kiu <zqdummy@gmail.com>
2021-04-28 23:19:54 -0700

    fix poster N/A situation

--- src/components/SearchResultComp/SearchResultComp.jsx
+++ src/components/SearchResultComp/SearchResultComp.jsx
@@ -1,13 +1,20 @@
 import React, {useState} from 'react'
 
+import defaultImage from '../../assets/images/poster_default.jpg'
+
 export default function SearchResultComp({searchResults}) {
 	const [resultPageNum, setResultPageNum] = useState(1)
 
 	let list = searchResults.map((movie) => (
 		<li key={movie.imdbID} className="resultList">
-			<img src={movie.Poster} alt="movie poster" />
+			<img
+				src={movie.Poster.toLowerCase() !== 'n/a' ? movie.Poster : defaultImage}
+				alt="movie poster"
+			/>
 			<h2>{movie.Title}</h2>
 			<p>{movie.Year}</p>
+			{/* // TODO: add feature to nominate */}
+			<button>Nominate</button>
 		</li>
 	))
 	return (
