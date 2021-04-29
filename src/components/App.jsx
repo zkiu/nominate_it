16f94ad6e382a449499c7d3819e5fd4593ff48de
Kiu <zqdummy@gmail.com>
2021-04-29 00:07:18 -0700

    add Banner comp skeleton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,19 @@
+import React, {useState} from 'react'
 import ToastComp from './ToastComp/ToastComp'
 
 import Header from './Header/Header'
+import Banner from './Banner/Banner'
 import Search from './Search/Search'
 
 // TODO: delete console & comments, remove unused modules
 
 export default function App() {
+	const [nominatedList, setNominatedList] = useState([])
+
 	return (
 		<main>
 			<Header />
+			<Banner nominatedList={nominatedList} />
 			<Search />
 			<ToastComp />
 		</main>
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import React, {useState} from 'react'
+
+export default function Banner({}) {
+	return (
+		<section>
+			<h1>Banner</h1>
+		</section>
+	)
+}
