224992fde07a1561bddc18e34cf1c67591850179
Kiu <zqdummy@gmail.com>
2021-04-30 19:55:29 -0700

    minor change

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -6,15 +6,12 @@ import SearchResultComp from '../SearchResultComp/SearchResultComp'
 export default function Search({addMovie, nominatedList}) {
 	const [searchResults, setSearchResults] = useState([])
 	const [totalPage, setTotalPage] = useState(1)
-	// const [resultPage, setResultPage] = useState(1)
 
 	return (
 		<section>
 			<SearchBarComp
 				setSearchResults={setSearchResults}
 				setTotalPage={setTotalPage}
-
-				// resultPage={resultPage}
 			/>
 			<SearchResultComp
 				searchResults={searchResults}
