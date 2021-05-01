e58c5a82daca12a4bb5927208bf01c3312278cce
Kiu <zqdummy@gmail.com>
2021-04-30 21:00:15 -0700

    style nominate

--- src/components/Nominate/Nominate.tsx
+++ src/components/Nominate/Nominate.tsx
@@ -3,23 +3,23 @@ import {Movie} from '../../services/typescript'
 
 export default function Nominate({nominatedList, removeMovie}) {
 	const list = nominatedList.map((movie: Movie, i: number) => (
-		<li key={movie.imdbID} className="">
-			<div className="">
-				<p>{`${i + 1}. `}</p>
-				<h2>{movie.Title}</h2>
-				<p>{movie.Year}</p>
-				<button
-					onClick={(e) => {
-						removeMovie(movie)
-					}}
-				>
-					Remove
-				</button>
-			</div>
+		<li key={movie.imdbID} className="item">
+			{/* <div className=""> */}
+			<p>{`${i + 1}. `}</p>
+			<h3>{movie.Title}</h3>
+			<p>{movie.Year}</p>
+			<button
+				onClick={(e) => {
+					removeMovie(movie)
+				}}
+			>
+				Remove
+			</button>
+			{/* </div> */}
 		</li>
 	))
 	return (
-		<section>
+		<section className="nominations">
 			<h2>Nominate 5 movies:</h2>
 			{nominatedList.length === 0 ? (
 				<h3>No movies nominated yet</h3>
