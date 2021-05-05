0c8c18c450909d0e2a65b09019bb17def45a13f7
Kiu <zqdummy@gmail.com>
2021-05-04 23:23:22 -0700

    done style for movie detail view

--- src/components/SearchResultItemComp/SearchResultItemComp.tsx
+++ src/components/SearchResultItemComp/SearchResultItemComp.tsx
@@ -1,3 +1,6 @@
+import {useState} from 'react'
+import axios from 'axios'
+
 import {Movie} from '../../services/typescript'
 import defaultImage from '../../assets/images/poster_default.jpg'
 
@@ -8,11 +11,31 @@ export default function SearchResultItemComp({
 	itemFocus,
 	setItemFocus,
 }) {
+	const [movieDetails, setMovieDetails] = useState({imdbRating: '', Plot: ''})
+	const [isLoading, setIsLoading] = useState(true)
+
 	const nominatedListID = nominatedList.map((movie: Movie) => movie.imdbID)
 
 	const handleClickFocus = (e, id) => {
-		setItemFocus(itemFocus === id ? null : id)
+		// setItemFocus(itemFocus === id ? null : id)
+		setIsLoading(true)
+		if (itemFocus === id) {
+			setItemFocus(null)
+			return
+		} else {
+			setItemFocus(id)
+			axios
+				.get(
+					`https://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&i=${id}`
+				)
+				.then(({data}) => {
+					setMovieDetails({imdbRating: data.imdbRating, Plot: data.Plot})
+					setIsLoading(false)
+				})
+				.catch((err) => console.error(err))
+		}
 	}
+
 	let list = searchResults.map((movie: Movie) => (
 		<li
 			key={movie.imdbID}
@@ -27,9 +50,19 @@ export default function SearchResultItemComp({
 			/>
 			<div className="infoContainer">
 				<div className="infoContainerText">
-					<h2>{movie.Title}</h2>
-					<p>{movie.Year}</p>
-					{movie.imdbID === itemFocus && <p>Movie Description</p>}
+					<h2 className="movieTitle">{movie.Title}</h2>
+					<p className="movieStat">
+						{movie.imdbID === itemFocus ? 'Released:' : null} {movie.Year}
+					</p>
+					{movie.imdbID === itemFocus &&
+						(isLoading === false ? (
+							<>
+								<p className="movieStat">Rating: {movieDetails.imdbRating}</p>
+								<p className="moviePlot">{movieDetails.Plot}</p>
+							</>
+						) : (
+							<p>Loading...</p>
+						))}
 				</div>
 				<button
 					disabled={nominatedListID.includes(movie.imdbID)}
