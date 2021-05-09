fce98207c5e1a7d3b0142935d703462e6875e81e
Kiu <zqdummy@gmail.com>
2021-05-08 22:55:56 -0700

    add animation to nominate list...
    need to fix issue where item gets moved to the
    bottom of the list as it dissappear

--- src/components/Nominate/Nominate.tsx
+++ src/components/Nominate/Nominate.tsx
@@ -1,19 +1,29 @@
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import {Movie} from '../../services/typescript'
 
 export default function Nominate({nominatedList, removeMovie}) {
 	const list = nominatedList.map((movie: Movie, i: number) => (
-		<li key={movie.imdbID} className="item">
-			<p>{`${i + 1}. `}</p>
-			<h3>{movie.Title}</h3>
-			<p>{movie.Year}</p>
-			<button
-				onClick={(e) => {
-					removeMovie(movie)
-				}}
-			>
-				Remove
-			</button>
-		</li>
+		<CSSTransition
+			key={`listItem-${i}`}
+			timeout={{
+				enter: 800,
+				exit: 500,
+			}}
+			classNames="item-animation-"
+		>
+			<li key={movie.imdbID} className="item">
+				<p>{`${i + 1}. `}</p>
+				<h3>{movie.Title}</h3>
+				<p>{movie.Year}</p>
+				<button
+					onClick={(e) => {
+						removeMovie(movie)
+					}}
+				>
+					Remove
+				</button>
+			</li>
+		</CSSTransition>
 	))
 	return (
 		<section className="nominations">
@@ -21,7 +31,9 @@ export default function Nominate({nominatedList, removeMovie}) {
 			{nominatedList.length === 0 ? (
 				<em className="emptyListMessage">No movies nominated yet</em>
 			) : (
-				<ol className="nominationContainer">{list}</ol>
+				<ol className="nominationContainer">
+					<TransitionGroup component={null}>{list}</TransitionGroup>
+				</ol>
 			)}
 		</section>
 	)
